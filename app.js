60d678eebb491a4c8331a10c53edcc7bc7544aff
Shashank290798 <shashanknalogal@gmail.com>
2022-11-09 19:35:13 +0530

    task3 commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('636b8e24903bcf11b028e2c6')
     .then(user => {
-      req.user = user;
+      req.user =new User(user.name, user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
