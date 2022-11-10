83b6e08e1dff0de2564da1b8d68838a364df2a14
Shashank290798 <shashanknalogal@gmail.com>
2022-11-10 17:18:28 +0530

    task12 commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 
 const Product = require('../models/product');
-const { populate } = require('../models/user');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -31,7 +31,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
@@ -73,6 +73,7 @@ exports.postCart = (req, res, next) => {
   });
 }
 exports.postCartDeleteProduct = (req, res, next) => {
+  
   const prodId = req.body.productId;
   req.user
     .removeFromCart(prodId)
@@ -83,10 +84,27 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-   .addOrder()
-   .then(result => {
+  .populate('cart.items.productId')
+  // .execPopulated()
+  .then(user => {
+    const products = user.cart.items.map(i=>{
+      return {quantity: i.quantity, product: {...i.productId._doc}};
+    });
+    const order = new Order({
+      user:{
+        name: req.user.name,
+        userId: req.user
+      },
+      products: products
+    });
+    return order.save();
+  })
+    .then(result => {
+      return req.user.clearCart();
+     
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
@@ -94,8 +112,7 @@ exports.postOrder = (req, res, next) => {
 
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({'user.userId': req.user._id})
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
